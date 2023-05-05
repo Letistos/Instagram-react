9ab17a1017d46da0c3ea0c1255882552f2101aa6
Leticia Santos Moura <letistos@gmail.com>
2023-05-05 13:15:55 -0300

    data test user

--- src/index.js
+++ src/index.js
@@ -213,11 +213,11 @@ function SideBar(){
 function User (){
     return(
         <div class="usuario">
-            <img src={require('./assets/kermit-cha.png')} alt="imagem de perfil" />
+            <img data-test="profile-image" src={require('./assets/kermit-cha.png')} alt="imagem de perfil" />
             <div class="texto">
-                <span>
+                <span data-test="name">
                     <strong>thekermit</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <ion-icon data-test="edit-name" name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
